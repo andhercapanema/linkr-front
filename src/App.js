16e912a40f19011758a37e7c90716984fe5acb78
andherpaulo <andher.dev@gmail.com>
2023-01-03 08:30:27 -0300

    feat: import fonts and config App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
 import GlobalStyle from "./assets/styles/GlobalStyle";
 
 function App() {
     return (
-        <div>
+        <BrowserRouter>
             <GlobalStyle />
-            App
-        </div>
+            Linkr
+        </BrowserRouter>
     );
 }
 
