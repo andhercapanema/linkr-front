bc98ce0200da518ef87ef6331f87970b2d83a988
dcaaz <danicreaazevedo@gmail.com>
2023-01-05 17:09:32 -0300

    feat: logic open&close vector, redirect to lognin

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,36 +1,67 @@
 import styled from "styled-components";
+import COLORS from "../../common/constants/colors";
 import logo from "../../assets/styles/Image/linkr.png";
 import up from "../../assets/styles/Image/up.png";
-import down from "../../assets/styles/Image/up.png";
+import down from "../../assets/styles/Image/down.png";
 import teste from "../../assets/styles/Image/teste.png";
-import COLORS from "../../common/constants/colors";
+import { useState } from "react";
+import { useNavigate } from "react-router";
 
 export default function Header() {
+
+    const [vector, setVector] = useState(down);
+    const [name, setName] = useState("down");
+
+    const navigate = useNavigate();
+
+    function logout() {
+        console.log("cheguei")
+
+        console.log("name", name)
+
+        if (name === "down") {
+            setVector(up);
+            setName("up");
+        } else {
+            setVector(down);
+            setName("down");
+        }
+    }
+
+    function logoutButton() {
+        navigate("/");
+    }
+
     return (
-        <Teste>
-            <left>
-                <img src={logo} alt="logo" />
-            </left>
-            <Right>
+        <>
+            <HeaderTimeline>
                 <h1>
-                    <img src={up} alt="vector" />
+                    <img src={logo} alt="logo" />
                 </h1>
-                <h2>
-                    <img src={teste} alt="teste" />
-                </h2>
-            </Right>
-        </Teste>
+                <Right>
+                    <h2 onClick={logout}>
+                        <img src={vector} alt={name} />
+                    </h2>
+                    <h3 onClick={logout}>
+                        <img src={teste} alt="teste" />
+                    </h3>
+                </Right>
+            </HeaderTimeline>
+            <Logout display={name}>
+                <button onClick={logoutButton}>Logout</button>
+            </Logout>
+        </>
     )
 }
 
-const Teste = styled.div`
+const HeaderTimeline = styled.div`
     background-color:  ${COLORS.accent1};
     width: 100vw;
     height: 72px;
     display: flex;
     justify-content: space-between;
     align-items: center;
-    left{
+    h1{
         margin-left: 2%;
         img{
         height: 50px;
@@ -41,16 +72,40 @@ const Teste = styled.div`
 const Right = styled.div`
     display: flex;
     margin-right: 2%;
-    h1{
+    h2{
         margin-right:10px;
         display: flex;
         align-items: center;
     }
-    h2{
+    h3{
         width: 52px;
         height: 52px;
         border-radius: 26.5px;
     }
 
 `
+
+const Logout = styled.div`
+    display: ${props => props.display == "down" ? "none" : "flex" };
+    justify-content: flex-end;
+    background-color:  ${COLORS.base};
+    button{
+        width: 150px;
+        height: 47px;
+        background-color: ${COLORS.accent1};
+        border-bottom-left-radius: 20px;
+        height: 47px;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        font-family: "Lato";
+        font-size: 20px;
+        font-weight: 700;
+        line-height: 20px;
+        letter-spacing: 0.05em;
+        color: #FFFFFF;
+    }
+    `
 
