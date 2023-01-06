e64e197fc9bc1fc017b69224d21808c9560e78e5
dcaaz <danicreaazevedo@gmail.com>
2023-01-06 13:56:17 -0300

    merge

--- src/pages/TimelinePage/TimelinePage.js
+++ src/pages/TimelinePage/TimelinePage.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import LinkrResources from "../../common/services/LinkrResources";
+import Post from "./Post";
+import { PostCard, PostForm, PostsList, StyledTimelinePage } from "./style";
 import Header from "../../components/Header/Header";
-import { PostCard, PostForm, StyledTimelinePage } from "./style";
 
 function TimelinePage() {
     const [form, setForm] = useState({
@@ -9,90 +10,120 @@ function TimelinePage() {
         description: "",
     });
     const { link, description } = form;
-    const [isLoading, setIsLoading] = useState(false);
+    const [formIsLoading, setFormIsLoading] = useState(false);
+    const [postsAreLoading, setPostsAreLoading] = useState(true);
     const userToken = "banana";
     const [posts, setPosts] = useState([]);
+    const [user, setUser] = useState({});
+
+    const getLoggedUserInfo = useCallback(async () => {
+        try {
+            const res = await LinkrResources.getLoggedUser(userToken);
+
+            setUser(res.data);
+        } catch (err) {
+            alert(err.response.data.message);
+            console.error(err.response);
+        }
+    }, []);
 
     function handleForm(e) {
         const { name, value } = e.target;
         setForm({ ...form, [name]: value });
     }
 
-    async function updateTimeline() {
+    const updateTimeline = useCallback(async () => {
         try {
-            const res = await LinkrResources.getPosts(userToken);
-            setPosts(res.data);
+            const res = await LinkrResources.getLastPosts(userToken);
+            const updatedPosts = res.data;
+
+            for (const post of updatedPosts) {
+                const res = await LinkrResources.getUser(
+                    post.user_id,
+                    userToken
+                );
+                post.user = res.data;
+
+                const metadata = await LinkrResources.getLinkMetadata(
+                    post.id,
+                    userToken
+                );
+                post.metadata = metadata.data;
+            }
+
+            setPosts(updatedPosts);
+            setPostsAreLoading(false);
         } catch (err) {
-            alert(err.response.data.message);
+            alert(
+                "An error occured while trying to fetch the posts, please refresh the page"
+            );
             console.error(err.response);
         }
-    }
+    }, []);
 
-    function submitPost(e) {
+    async function submitPost(e) {
         e.preventDefault();
-        setIsLoading(true);
+        setFormIsLoading(true);
 
         try {
-            LinkrResources.postNewPost(form, userToken);
+            await LinkrResources.postNewPost(form, userToken);
 
-            setIsLoading(false);
             setForm({ link: "", description: "" });
-            updateTimeline();
+            await updateTimeline();
+            setFormIsLoading(false);
         } catch (err) {
             alert("There was an error publishing your link");
             console.error(err.response);
-            setIsLoading(false);
+            setFormIsLoading(false);
         }
     }
 
     useEffect(() => {
         updateTimeline();
-    }, []);
+        getLoggedUserInfo();
+    }, [updateTimeline, getLoggedUserInfo]);
 
     return (
-        <>
-            <Header />
-            <StyledTimelinePage>
-                <h2>timeline</h2>
-                <PostCard>
-                    {window.screen.width >= 611 ? (
-                        <img
-                            alt="User profile"
-                            src="https://images2.alphacoders.com/495/495160.png"
-                        />
-                    ) : (
-                        ""
-                    )}
-                    <PostForm onSubmit={submitPost} isLoading={isLoading}>
-                        <h3>What are you going to share today?</h3>
-                        <input
-                            type="url"
-                            placeholder="http:// ..."
-                            name="link"
-                            onChange={handleForm}
-                            value={link}
-                            required
-                            disabled={isLoading}
-                        />
-                        <input
-                            type="text"
-                            placeholder="Awesome article about #javascript"
-                            name="description"
-                            onChange={handleForm}
-                            value={description}
-                            disabled={isLoading}
-                        />
-                        <div>
-                            {isLoading ? (
-                                <button disabled>Publishing...</button>
-                            ) : (
-                                <button>Publish</button>
-                            )}
-                        </div>
-                    </PostForm>
-                </PostCard>
-            </StyledTimelinePage>
-        </>
+        <StyledTimelinePage>
+            <h2>timeline</h2>
+            <PostCard>
+                {window.screen.width >= 611 ? (
+                    <img
+                        alt="User profile"
+                        src="https://images2.alphacoders.com/495/495160.png"
+                    />
+                ) : (
+                    ""
+                )}
+                <PostForm onSubmit={submitPost} isLoading={isLoading}>
+                    <h3>What are you going to share today?</h3>
+                    <input
+                        type="url"
+                        placeholder="http:// ..."
+                        name="link"
+                        onChange={handleForm}
+                        value={link}
+                        required
+                        disabled={isLoading}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Awesome article about #javascript"
+                        name="description"
+                        onChange={handleForm}
+                        value={description}
+                        disabled={isLoading}
+                    />
+                    <div>
+                        {isLoading ? (
+                            <button disabled>Publishing...</button>
+                        ) : (
+                            <button>Publish</button>
+                        )}
+                    </div>
+                </PostForm>
+            </PostCard>
+        </StyledTimelinePage>
     );
 }
 
--- src/pages/TimelinePage/style.js
+++ src/pages/TimelinePage/style.js
@@ -23,6 +23,26 @@ export const StyledTimelinePage = styled.main`
             padding: 78px 0 43px;
         }
     }
+
+    img {
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        object-fit: cover;
+
+        @media (min-width: 611px) {
+            width: 50px;
+            height: 50px;
+        }
+    }
+
+    h4 {
+        font-family: "Lato";
+        text-align: center;
+        font-weight: 500;
+        font-size: 25px;
+        margin-top: 15px;
+    }
 `;
 
 export const PostCard = styled.div`
@@ -40,11 +60,7 @@ export const PostCard = styled.div`
     }
 
     img {
-        width: 50px;
-        height: 50px;
-        border-radius: 50%;
         margin-right: 18px;
-        object-fit: cover;
     }
 `;
 
@@ -111,5 +127,174 @@ export const PostForm = styled.form`
             font-size: 13px;
             line-height: 16px;
         }
+    }
+`;
+
+export const PostsList = styled.ul`
+    margin-top: 16px;
+    width: 100%;
+    font-family: "Lato";
+`;
+
+export const StyledPost = styled.li`
+    background-color: ${COLORS.accent1};
+    margin-bottom: 16px;
+    padding: 12px 16px;
+    display: flex;
+
+    @media (min-width: 611px) {
+        border-radius: 16px;
+        padding: 20px;
+    }
+`;
+
+export const LikesColumn = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-right: 14px;
+
+    @media (min-width: 611px) {
+        margin-right: 19px;
+    }
+
+    ion-icon {
+        font-size: 17px;
+        margin: 17px 0 12px;
+        cursor: pointer;
+
+        @media (min-width: 611px) {
+            font-size: 20px;
+            margin: 19px 0 4px;
+        }
+    }
+
+    p {
+        font-size: 9px;
+        line-height: 11px;
+        text-align: center;
+
+        @media (min-width: 611px) {
+            font-size: 11px;
+            line-height: 13px;
+        }
+    }
+`;
+
+export const PostInfos = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+
+    input {
+        margin-bottom: 8px;
+        padding: 5px 10px;
+        border-radius: 7px;
+        border: none;
+        width: 100%;
+
+        font-size: 14px;
+        line-height: 17px;
+        color: #4c4c4c;
+    }
+
+    p {
+        font-size: 15px;
+        line-height: 18px;
+        color: ${COLORS.text3};
+        margin-bottom: 13px;
+
+        @media (min-width: 611px) {
+            font-size: 17px;
+            line-height: 20px;
+        }
+    }
+`;
+
+export const UsernameEditDelete = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 7px;
+
+    h4 {
+        margin-top: 0;
+        font-weight: 400;
+        text-align: start;
+
+        font-size: 17px;
+        line-height: 20px;
+
+        @media (min-width: 611px) {
+            font-size: 19px;
+            line-height: 23px;
+        }
+    }
+
+    ion-icon {
+        font-size: 17px;
+        margin-left: 12px;
+        cursor: pointer;
+
+        @media (min-width: 611px) {
+            font-size: 19px;
+        }
+    }
+`;
+
+export const StyledLinkSnippet = styled.a`
+    display: flex;
+    text-decoration: none;
+    border: 1px solid #4d4d4d;
+    border-radius: 11px;
+
+    div {
+        padding: 8px 0 8px 11px;
+        width: 67%;
+
+        @media (min-width: 611px) {
+            padding: 24px 0 24px 20px;
+        }
+
+        h5 {
+            font-size: 11px;
+            line-height: 13px;
+            color: ${COLORS.text4};
+            margin-bottom: 4px;
+
+            @media (min-width: 611px) {
+                font-size: 16px;
+                line-height: 19px;
+            }
+        }
+
+        p,
+        h6 {
+            font-size: 9px;
+            line-height: 11px;
+
+            @media (min-width: 611px) {
+                font-size: 11px;
+                line-height: 13px;
+            }
+        }
+
+        p {
+            color: ${COLORS.text5};
+            margin-bottom: 4px;
+
+            @media (min-width: 611px) {
+                margin-bottom: 13px;
+            }
+        }
+
+        h6 {
+            color: ${COLORS.text4};
+        }
+    }
+
+    img {
+        width: 33%;
+        height: 100%;
+        border-radius: 0px 11px 11px 0px;
     }
 `;
