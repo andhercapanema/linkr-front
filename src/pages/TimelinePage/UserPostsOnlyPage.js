44216b35cc238a5c5ea23ad7eb197cf6bc861b78
dcaaz <danicreaazevedo@gmail.com>
2023-01-08 18:35:42 -0300

    feat: single user screen logic check

--- src/pages/TimelinePage/UserPostsOnlyPage.js
+++ src/pages/TimelinePage/UserPostsOnlyPage.js
@@ -1,13 +1,33 @@
 import styled from "styled-components";
 import { StyledTimelinePage, PostsList } from "./style";
-import Post from "./Post";
 import { AuthContext } from "../../Ayth";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import Header from "../../components/Header/Header";
+import axios from "axios";
+import { useState } from "react";
+import { StyledPost, LikesColumn, StyledLinkSnippet } from "./style";
+import COLORS from "../../common/constants/colors";
 
 export default function UserPostsOnly() {
 
-    const { posts, user } = useContext(AuthContext);
+    const { user } = useContext(AuthContext);
+    const [data, setData] = useState([]);
+
+    const id = user.id
+    console.log("data", data)
+
+    useEffect(() => {
+        axios
+            .get(`http://localhost:4000/search/${id}`)
+            .then((res) => {
+                console.log("res", res.data)
+                setData(res.data);
+            })
+            .catch((err) => {
+                console.log("err userPostsOnlyPag", err);
+            });
+    }, [id]);
+
 
     return (
         <>
@@ -18,17 +38,37 @@ export default function UserPostsOnly() {
                     <h1>{user.username}'s posts</h1>
                 </Title>
                 <PostsList>
-                    {/* {posts.length === 0 ? (
-                        <h4>There are no posts yet</h4>
-                    ) : (
-                        posts.map((post) => (
-                            <Post
-                                key={post.id}
-                                post={post}
-                                username={username}
-                            />
-                        ))
-                    )} */}
+                    {data.map((teste) => (
+
+                        <StyledPost>
+                            <LikesColumn>
+                                <img alt="User profile" src={user.picture_url} />
+                                <ion-icon name="heart-outline"></ion-icon>
+                                <p>{teste.likesAmount} likes</p>
+                            </LikesColumn>
+                            <Teste>
+                                <Username>
+                                    <h1>{user.username}</h1>
+                                </Username>
+                                <PostInfo>
+                                    <h1>{teste.description}</h1>
+                                </PostInfo>
+                                <StyledLinkSnippet href={teste.link} target="_blank" rel="noreferrer">
+                                    <div>
+                                        <h5>a</h5>
+                                        <p>b</p>
+                                        <h6>c</h6>
+                                    </div>
+                                    {/* <div>
+                                        <h5>{title}</h5>
+                                        <p>{description}</p>
+                                        <h6>{url}</h6>
+                                    </div> */}
+                                    {/* <img alt="Link" src={image} /> */}
+                                </StyledLinkSnippet>
+                            </Teste>
+                        </StyledPost>
+                    ))}
                 </PostsList>
             </StyledTimelinePage>
         </>
@@ -50,5 +90,34 @@ const Title = styled.div`
         letter-spacing: 0em;
         text-align: left;
 
+    }
+`
+
+const Teste = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+const Username = styled.div`
+    h1{
+        font-weight: 400;
+        font-size: 17px;
+        line-height: 20px;
+    }
+`
+
+const PostInfo = styled.div`
+        margin-top: 5px;
+
+    h1{
+        font-size: 15px;
+        line-height: 18px;
+        color: ${COLORS.text3};
+        margin-bottom: 13px;
+
+        @media (min-width: 611px) {
+            font-size: 17px;
+            line-height: 20px;
+        }
     }
 `
